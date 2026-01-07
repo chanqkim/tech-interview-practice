9e9a0996faacf2908b4b6da9085c7fa8f1e969fa
ChanKyu Kim <chk.kim87@gmail.com>
2026-01-07 17:24:32 +0900

    feat(leetcode): add queue simulation solution for 2073_time_needed_to_buy_tickets

---
+++ data_structures/queue/simulation/2073_time_needed_to_buy_tickets.py
@@ -0,0 +1,47 @@
+"""
+link: https://leetcode.com/problems/time-needed-to-buy-tickets/description/?envType=problem-list-v2&envId=dsa-sequence-valley-queue
+
+
+"""
+
+from collections import deque
+from typing import List
+
+
+# Time Complexity: O(sum(tickets))
+#   - Each ticket purchase takes 1 second
+#   - Total operations equal the total number of tickets sold
+# Space Complexity: O(n)
+#   - Queue stores up to n people
+class Solution1:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        # Initialize a queue to simulate the line
+        # Each element is a tuple: (original_index, remaining_tickets)
+        queue = deque()
+        for i in range(len(tickets)):
+            queue.append((i, tickets[i]))  # Store index to identify person k later
+
+        # This variable tracks the total elapsed time (seconds)
+        answer = 0
+
+        # Continue simulation while there are people in the queue
+        while queue:
+            # Take the person at the front of the queue
+            index, remaining_tickets = queue.popleft()
+
+            # The person buys exactly one ticket (1 second passes)
+            remaining_tickets -= 1
+            answer += 1
+
+            # If this person is k and they just bought their last ticket,
+            # we stop the simulation immediately
+            if remaining_tickets == 0 and index == k:
+                break
+
+            # If the person still needs more tickets,
+            # they go back to the end of the queue
+            if remaining_tickets > 0:
+                queue.append((index, remaining_tickets))
+
+        # Return the total time until person k finishes buying tickets
+        return answer
